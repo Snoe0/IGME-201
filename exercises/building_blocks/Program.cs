e6cdac623057bf01898be2cd99b2efc1f4353df4
Snoe <yurikorolev99@gmail.com>
2024-10-03 09:10:00 -0400

    building blocks assignment

---
+++ exercises/building_blocks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Week5aIGME201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumberOne = 7;
+            int myNumberTwo = 27;
+            ProfName();
+            Console.WriteLine("Hello, professor!");
+            Console.WriteLine(myNumberOne);
+            Console.WriteLine(myNumberTwo);
+            Console.WriteLine(Sum(myNumberOne, myNumberTwo));
+            
+        }
+
+        static void ProfName()
+        {
+            string profName = "Nick Buonarota";
+            Console.WriteLine(profName);
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return(num1 + num2);
+        }
+    }
+}
