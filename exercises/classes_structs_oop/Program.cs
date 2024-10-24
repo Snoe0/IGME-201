4e8e8fa9974406e16abe98557b155a9e02bec66a
Snoe <yurikorolev99@gmail.com>
2024-10-24 08:30:18 -0400

    created Character class and instantiated it

--- exercises/classes_structs_oop/Program.cs
+++ exercises/classes_structs_oop/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        public class Character
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Character hero = new Character();
         }
     }
 }
